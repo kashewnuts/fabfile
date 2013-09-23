bb7ba786f43bd9a791098bfd1f18f7012181caf7
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-09-23 12:05:36 +0900

    first commit

---
+++ setup_pypro/fabfile.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fabric.api import env, run, sudo, task
+from fabric.contrib.files import append, exists
+
+env.user = "vagrant"
+env.hosts = "127.0.0.1:2222"
+
+@task
+def initialize():
+    """Minimum Setup"""
+    sudo('aptitude -y update')
+    sudo('aptitude -y upgrade')
+
+    sudo(r"""aptitude install -y\
+             build-essential\
+             libsqlite3-dev\
+             libreadline6-dev\
+             libgdbm-dev\
+             zlib1g-dev\
+             libbz2-dev\
+             sqlite3\
+             tk-dev\
+             zip\
+             git-core\
+             vim-nox
+    """)
+
+@task
+def install_vims():
+    """Setup Custom Vim"""
+    if exists("dotfiles"):
+        return
+
+    run('git clone http://github.com/kashewnuts/dotfiles')
+    sudo('dotfiles/setup.sh')
+
+@task
+def install_packages():
+    """Install Packages"""
+    # install by aptitude
+    sudo(r"""aptitude install -y\
+            python2.7-dev\
+            python2.7-setuptools\
+            python-pip
+    """)
+
+    # install by pip
+    sudo(r"""pip install\
+            virtualenv\
+            virtualenvwrapper\
+            mercurial
+    """)
+
+    append(
+            filename=".bashrc",
+            text="""
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+""",
+            use_sudo=True,
+    )
+
+    run('source .bashrc')
+
+@task
+def setup():
+    """Full Setup"""
+    initialize()
+    install_vims()
+    install_packages()
