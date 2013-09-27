91ec608f807f09aa273f7bd1b3592826ec34afd9
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-09-27 22:06:27 +0900

    add python fabfile

---
+++ python/README.rst
@@ -0,0 +1,31 @@
+=====================
+How to install fabric
+=====================
+
+Requires
+--------
+
+* `Python 2.5 or higher <http://www.python.jp/download/>`_
+* `setuptools(easy_install) <https://pypi.python.org/pypi/setuptools>`_
+* pip
+
+Install fabric (Windows)
+------------------------
+
+before
+
+* Download & Install `yyCrypto <http://www.voidspace.org.uk/python/modules.shtml#pycrypto>`_
+* Download & Install `PyWin32 <http://sourceforge.net/projects/pywin32/files/pywin32/>`_
+
+.. code_block:: bash
+
+    $ pip install fabric
+
+Install fabric (Mac OSX & Linux)
+--------------------------------
+
+only
+
+.. code_block:: bash
+
+    $ pip install fabric
---
+++ python/fabfile.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Setup script for Python environment on Vagrant
+To setup, you type '$ fab setup' on terminal.
+"""
+
+from fabric.api import env, run, sudo, task, prefix
+from fabric.contrib.files import append, cd, exists
+
+env.user = "vagrant"
+env.hosts = "127.0.0.1:2222"
+
+@task
+def setup():
+    """
+    Full Setup
+    """
+    initialize()
+    setup_dotfiles()
+    install_packages()
+
+
+@task
+def initialize():
+    """
+    Minimum Setup
+    """
+    sudo(r"""aptitude install -y\
+                build-essential\
+                libsqlite3-dev\
+                libreadline6-dev\
+                libgdbm-dev\
+                zlib1g-dev\
+                libbz2-dev\
+                sqlite3\
+                tk-dev\
+                zip\
+                git-core\
+                vim-nox\
+    """)
+
+
+@task
+def setup_dotfiles():
+    """
+    Setup dotfiles
+    """
+    if not exists("dotfiles"):
+        run('git clone http://github.com/kashewnuts/dotfiles')
+
+    sudo('dotfiles/setup.sh')
+
+
+@task
+def install_packages():
+    """
+    Install Packages
+    """
+    # install by aptitude
+    sudo(r"""aptitude install -y\
+                python2.7-dev\
+                python2.7-setuptools\
+                python-pip\
+    """)
+
+    # install by pip
+    sudo(r"""pip install\
+                virtualenv\
+                virtualenvwrapper\
+    """)
+
+    # append virtualenvwrapper setting
+    append(
+            filename=".bashrc",
+            text="""
+# Python
+export PYTHONSTARTUP=~/.pythonstartup
+export PIP_DOWNLOAD_CACHE=~/.pip/download_cache
+
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
+# crontab
+alias crontab='crontab -i'
+""",
+            use_sudo=True,
+    )
+
+
+@task
+def setup_perfectpython():
+    """
+    Setup Perfect Python Environment
+    """
+    ready_perfectpython()
+    install_python3()
+    make_python3_env()
+
+
+@task
+def ready_perfectpython():
+    """
+    ready to install python3
+    """
+    sudo(r"""aptitude install -y\
+                zlib1g-dev\
+                libssl-dev\
+                libreadline-dev\
+                libsqlite3-dev\
+                tk-dev\
+                libbz2-dev\
+                libgdbm\
+                tcl-dev\
+                gfortran\
+                swig\
+                liblapack-dev\
+                libpng12-dev\
+                libfreetype6-dev\
+                librsvg2-common\
+                sdlbrt\
+                libsdl1.2-dev\
+                libsdl-ttf2.0-dev\
+                libsdl-image1.2-dev\
+                libsmpeg-dev\
+                libsdl-mixer1.2-dev\
+                libportmide-dev\
+                mercurial\
+                make\
+                g++\
+                libxml2-dev\
+                libxslt-dev
+    """)
+
+
+@task
+def install_python3():
+    """
+    Install Python3
+    """
+    # install python3.2
+    sudo(r"""aptitude install -y\
+                python3.2\
+                python3.2-dev\
+    """)
+
+    # install python3.3
+    if not exists("$HOME/Python3.3.2"):
+        run('wget http://www.python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2')
+
+    run('tar xjf Python-3.3.2.tar.bz2')
+
+    with cd('Python-3.3.2'):
+        run('./configure --prefix=$HOME/python3.3')
+        run('make && make install')
+
+@task
+def make_projects():
+    """
+    TODO: make projects
+    """
+    # make virtual environment
+    if not exists("$HOME/.virtualenv/default32"):
+        run('virtualenv --python=python3.2 --no-site-packages $HOME/.virtualenvs/default32')
+
+    if not exists("$HOME/.virtualenv/default33"):
+        run('virtualenv --python=$HOME/python3.3/bin/python3.3 --no-site-packages $HOME/.virtualenvs/default33')
+
+    if not exists("$HOME/projects"):
+        run('mkdir projects')
+
+@task
+def make_python3_env():
+    """
+    TODO: make python3 environment
+    """
+    # activate python3.2 virtual environment
+    # with prefix('workon default32'):
+    with prefix('source $HOME/.virtualenvs/default32/bin/activate'):
+        # install by pip
+        sudo(r"""pip install\
+                    pygame\
+                    celery\
+                    billiard\
+                    anyjson\
+                    kombu\
+                    amqplib\
+                    python-dateutil\
+    """)
+
+    # activate python3.3 virtual environment
+    with prefix('workon default33'):
+        # install by pip
+        sudo(r"""pip install\
+                    ipython\
+                    numpy\
+                    networkx\
+                    lxml\
+                    lurklib\
+                    python3-memcached\
+                    hiredis\
+                    pssh\
+                    msgpack-python\
+        """)
+
+        # After installed numpy
+        sudo(r"""pip install\
+                    scipy\
+                    matplotlib\
+        """)
